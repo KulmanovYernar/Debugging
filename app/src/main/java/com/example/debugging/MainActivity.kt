9043168c4575ed69a4d735199a2e3ac6ba310327
yerakulmanov <200103127@stu.sdu.edu,.kz>
2023-04-27 15:39:37 +0600

    Record a running app

--- app/src/main/java/com/example/debugging/MainActivity.kt
+++ app/src/main/java/com/example/debugging/MainActivity.kt
@@ -3,23 +3,32 @@ package com.example.debugging
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.widget.TextView
+import kotlin.concurrent.thread
 
 private const val TAG = "MainActivity"
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+//        Log.d(TAG, "this is where the app crashed before")
+//        val helloTextView: TextView = findViewById(R.id.division_textview)
+//        Log.d(TAG, "this should be logged if the bug is fixed")
+//        helloTextView.text = "Hello, debugging!"
         logging()
         division()
     }
-    fun division(){
+    fun division() {
         val numerator = 60
         var denominator = 4
-
-        repeat(4){
-            Log.d(TAG, "$denominator")
-            Log.v(TAG,"${numerator / denominator}")
-            denominator--
+        thread(start = true) {
+            repeat(4) {
+                Thread.sleep(3000)
+                runOnUiThread {
+                    findViewById<TextView>(R.id.division_textview).setText("${numerator / denominator}")
+                    denominator--
+                }
+            }
         }
     }
 
